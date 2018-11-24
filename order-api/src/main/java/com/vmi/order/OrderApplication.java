611a2897db6ff3484e7638b93ef7630d2e54593c
Valeriy <vmironichev@chegg.com>
2018-11-24 11:34:09 +0200

    Add swagger 2 support

--- order-api/src/main/java/com/vmi/order/OrderApplication.java
+++ order-api/src/main/java/com/vmi/order/OrderApplication.java
@@ -1,9 +1,12 @@
 package com.vmi.order;
 
+import com.vmi.phone.swagger.CommonSwaggerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(CommonSwaggerConfiguration.class)
 public class OrderApplication {
 
 	public static void main(String[] args) {
--- phone-catalog-api/src/main/java/com/vmi/phone/catalog/PhoneCatalogApplication.java
+++ phone-catalog-api/src/main/java/com/vmi/phone/catalog/PhoneCatalogApplication.java
@@ -1,10 +1,13 @@
 package com.vmi.phone.catalog;
 
+import com.vmi.phone.swagger.CommonSwaggerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.vmi.phone")
+@Import(CommonSwaggerConfiguration.class)
 @EnableAutoConfiguration
 public class PhoneCatalogApplication {
 
